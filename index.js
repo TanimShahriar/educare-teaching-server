6812769a8ae27397651cd0df8e769c1d445dda9f
TanimShahriar <ssmtsshr@gmail.com>
2023-12-17 15:16:33 +0600

    courseCollection database created

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const express = require("express");
+//initializing
+const express = require('express');
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -8,9 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.6mxxl.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6mxxl.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,9 +26,24 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+
+
+    const courseCollection = client.db("educareDB").collection("courses");
+
+
+    //course related api
+    app.get("/courses", async (req, res) => {
+      const cursor = courseCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+
+
+
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -37,9 +54,9 @@ run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
-  res.send("educare is sitting")
+  res.send("Educare teaching server is running");
 })
 
 app.listen(port, () => {
-  console.log(`Educare is sitting on port ${port}`)
+  console.log((`Educare teaching server is running on port: ${port}`))
 })
